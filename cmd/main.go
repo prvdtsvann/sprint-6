54a5aa60b6df57ade6acda760f99441162f28bf8
Анна Приведенцева <annaprivedenceva@MacBook-Air-Anna.local>
2025-07-22 19:41:51 +0700

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,10 @@ import (
 
 func main() {
 	logger := log.New(os.Stdout, "server: ", log.LstdFlags)
-	srv := server.NewRouter(logger)
-	logger.Printf("Server is running on: %s", srv.HTTPServer.Addr)
 
+	srv := server.NewRouter(logger)
+
+	logger.Printf("Server is running on: %s", srv.HTTPServer.Addr)
 	err := srv.HTTPServer.ListenAndServe()
 	if err != nil {
 		logger.Fatalf("Server error: %v", err)
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(input string) string {
+	if input == "" {
+		return ""
+	}
+
+	isMorse := true
+	for _, v := range input {
+		if v != '.' && v != '-' && v != ' ' {
+			isMorse = false
+			break
+		}
+	}
+	if isMorse {
+		return morse.ToText(input)
+	}
+	return morse.ToMorse(input)
+}
